8942c87ecdec7033fe7db089d540b2c1ac608145
akkumar-qlik <akarsh.kumar@qlik.com>
2026-02-23 10:08:41 +0530

    Added minor changes

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     classifiers=["Programming Language :: Python :: 3 :: Only"],
     py_modules=["tap_codat"],
     install_requires=[
-        "singer-python==6.7.0,
+        "singer-python==6.7.0",
         "requests==2.32.5",
         "pendulum==3.1.0"
     ],
